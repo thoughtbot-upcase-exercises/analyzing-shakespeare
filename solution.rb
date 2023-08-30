09b650c9aca0d2f5d672f51ef442bc421708add2
Andy Deng <denga@bc.edu>
2023-08-30 18:31:26 -0400

    small change

--- solution.rb
+++ solution.rb
@@ -1,12 +1,10 @@
 require 'nokogiri'
 
-# Open and parse the XML file
 xml_file = File.open("macbeth.xml")
 xml_doc = Nokogiri::XML(xml_file)
 
 character_lines = Hash.new(0)
 
-# Do something with the XML data
 xml_doc.xpath('//SPEECH').each do |speech|
     speaker = speech.at_xpath('SPEAKER').text
     line = speech.at_xpath('LINE').text
@@ -18,6 +16,4 @@ character_lines.sort_by {|k,v| v}.reverse.each do |k,v|
     end
 
 
-
-# Close the XML file
 xml_file.close
