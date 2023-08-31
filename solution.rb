cf3584eb2e7f4201312049c269df8045a9810345
Andy Deng <denga@bc.edu>
2023-08-30 21:36:37 -0400

    pr changes

--- solution.rb
+++ solution.rb
@@ -1,19 +1,24 @@
 require 'nokogiri'
+require 'open-uri'
 
-xml_file = File.open("macbeth.xml")
+# open from internet instead
+xml_file = URI.open('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
 xml_doc = Nokogiri::XML(xml_file)
 
 character_lines = Hash.new(0)
 
 xml_doc.xpath('//SPEECH').each do |speech|
     speaker = speech.at_xpath('SPEAKER').text
-    line = speech.at_xpath('LINE').text
-    character_lines[speaker] += 1
-  end
-
-character_lines.sort_by {|k,v| v}.reverse.each do |k,v|
-    puts "#{k.downcase.capitalize} #{v}"
+    if speaker == "ALL"
+        next
     end
+    lines = speech.xpath('LINE')
+    character_lines[speaker] += lines.count
+end
+
+character_lines.sort_by {|name, num_lines| num_lines}.reverse.each do |name ,num_lines|
+    puts "#{name.downcase.capitalize} #{num_lines}"
+end
 
 
 xml_file.close
